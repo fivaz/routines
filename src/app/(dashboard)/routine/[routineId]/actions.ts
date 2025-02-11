5c13b0d1c8bd7308ae7afbb991a6ef9e72457563
fivaz <s.a.fivaz@gmail.com>
2025-02-11 02:10:15 +0100

    implement recraft api integration

--- src/app/(dashboard)/routine/[routineId]/actions.ts
+++ src/app/(dashboard)/routine/[routineId]/actions.ts
@@ -1,8 +1,29 @@
 'use server';
 
-export async function generateImage(prompt: string) {
-	await new Promise((resolve) => setTimeout(resolve, 1000));
+import OpenAI from 'openai';
 
-	// Return a mock image URL
-	return { imageUrl: '/mock.png' };
+const client = new OpenAI({
+	apiKey: process.env.RECRAFT_API_TOKEN,
+	baseURL: 'https://external.api.recraft.ai/v1',
+});
+
+export async function generateImage(prompt: string) {
+	console.log('prompt', prompt);
+	console.log('process.env.RECRAFT_API_STYLE:', process.env.RECRAFT_API_STYLE);
+	// await new Promise((resolve) => setTimeout(resolve, 1000));
+	//
+	// // Return a mock image URL
+	// return { imageUrl: '/mock.png' };
+	try {
+		const response = await client.images.generate({
+			prompt,
+			style_id: process.env.RECRAFT_API_STYLE,
+		});
+
+		const imageUrl = response.data[0].url;
+		return { imageUrl };
+	} catch (error) {
+		console.error('Error:', error);
+		throw new Error('Failed to generate image');
+	}
 }
