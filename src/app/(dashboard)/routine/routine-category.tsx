23809b14e5013dd219609be470f1e12c4a2b76e8
fivaz <s.a.fivaz@gmail.com>
2025-05-22 06:07:27 +0200

    refactor

--- src/app/(dashboard)/routine/routine-category.tsx
+++ src/app/(dashboard)/routine/routine-category.tsx
@@ -1,8 +1,10 @@
-import { PropsWithChildren } from 'react';
+import { MouseEvent, PropsWithChildren } from 'react';
 import { Subheading } from '@/components/base/heading';
 import { CollisionPriority } from '@dnd-kit/abstract';
 import { useSortable } from '@dnd-kit/react/sortable';
 import { Category } from '@/lib/category/category.type';
+import { GripVerticalIcon } from 'lucide-react';
+import { Button } from '@/components/base/button';
 
 export function RoutineCategory({
 	category,
@@ -20,10 +22,23 @@ export function RoutineCategory({
 	const style = isDropTarget ? { background: '#00000030' } : undefined;
 
 	return (
-		<div className="p-4 border border-gray-200 rounded-lg" style={style} ref={ref}>
-			<Subheading className="mb-3 text-yellow-600 first-letter:uppercase">
-				{category.name}
-			</Subheading>
+		<div
+			className="flex gap-2 flex-col p-4 border border-gray-200 rounded-lg"
+			style={style}
+			ref={ref}
+		>
+			<div className="flex gap-2 items-center">
+				<Button
+					className="touch-none dark cursor-grab"
+					onClick={(e: MouseEvent<HTMLButtonElement>) => {
+						e.stopPropagation(); // Prevent link click when button is clicked
+					}}
+				>
+					<GripVerticalIcon className="size-5" />
+				</Button>
+
+				<Subheading className="text-yellow-600 first-letter:uppercase">{category.name}</Subheading>
+			</div>
 			<div className="flex flex-col gap-2 min-h-45">{children}</div>
 		</div>
 	);
