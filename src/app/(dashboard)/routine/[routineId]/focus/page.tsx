4bb1f81775c7244975c8ee3d16d52e14fd947b63
fivaz <31249205+fivaz@users.noreply.github.com>
2025-08-27 20:24:40 +0200

    fix problem with tasks id not being 0 when routine changes

--- src/app/(dashboard)/routine/[routineId]/focus/page.tsx
+++ src/app/(dashboard)/routine/[routineId]/focus/page.tsx
@@ -4,13 +4,16 @@ import { RoutineTasksSummary } from '@/app/(dashboard)/routine/[routineId]/focus
 import { TaskImage } from '@/app/(dashboard)/routine/[routineId]/focus/TaskImage';
 import { TaskInfo } from '@/app/(dashboard)/routine/[routineId]/focus/task-info/TaskInfo';
 import { FocusController } from '@/app/(dashboard)/routine/[routineId]/focus/focus-controller/FocusController';
-import { useSetAtom } from 'jotai';
+import { useAtomValue, useSetAtom } from 'jotai';
 import { dateAtom } from '@/lib/session/session.type';
 import { getToday } from '@/lib/session/session.utils';
+import { resetTaskIndexOnRoutineChangeEffect } from '@/app/(dashboard)/routine/[routineId]/focus/service';
 
 export default function RoutineFocusPage() {
 	const setDate = useSetAtom(dateAtom);
 	setDate(getToday());
+	// Mount effect to reset task index whenever routineId changes
+	useAtomValue(resetTaskIndexOnRoutineChangeEffect);
 
 	return (
 		<div className="flex h-full flex-col justify-between gap-5 md:h-[calc(100vh-136px)]">
