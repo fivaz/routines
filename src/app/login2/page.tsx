8c27448f8e0dcd37233f24e19b6dc39e4b1db5ff
fivaz <31249205+fivaz@users.noreply.github.com>
2025-09-06 12:14:59 +0200

    refactor

--- src/app/login2/page.tsx
+++ src/app/login2/page.tsx
@@ -7,8 +7,10 @@ import { Logo } from '@/components/Logo';
 import { Button } from '@/components/base/button';
 import { Banner } from '@/components/base/banner';
 import Link from 'next/link';
-import ThemeToggle2, { ThemeToggle3 } from '@/components/shared/ThemeToggle2';
+import { ThemeToggle3 } from '@/components/shared/ThemeToggle2';
 import useRouterWithQuery from '@/lib/utils.hook';
+import { Input } from '@/components/base/input';
+import { Field, Label } from '@/components/base/fieldset';
 
 export default function Login() {
 	return (
@@ -31,43 +33,15 @@ export default function Login() {
 						{/*<Banner setMessage={setErrorMessage}>{errorMessage}</Banner>*/}
 						<div className="py-12">
 							<form className="space-y-6">
-								<div>
-									<label
-										htmlFor="email"
-										className="block text-sm/6 font-medium text-gray-900 dark:text-white"
-									>
-										Email address
-									</label>
-									<div className="mt-2">
-										<input
-											id="email"
-											name="email"
-											type="email"
-											required
-											autoComplete="email"
-											className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6"
-										/>
-									</div>
-								</div>
+								<Field>
+									<Label>Email address</Label>
+									<Input name="email" type="email" required autoComplete="email" />
+								</Field>
 
-								<div>
-									<label
-										htmlFor="password"
-										className="block text-sm/6 font-medium text-gray-900 dark:text-white"
-									>
-										Password
-									</label>
-									<div className="mt-2">
-										<input
-											id="password"
-											name="password"
-											type="password"
-											required
-											autoComplete="current-password"
-											className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6"
-										/>
-									</div>
-								</div>
+								<Field>
+									<Label>Password</Label>
+									<Input name="password" type="password" required autoComplete="current-password" />
+								</Field>
 
 								<div className="flex items-center justify-between">
 									<div className="flex gap-3">
@@ -124,32 +98,24 @@ export default function Login() {
 							</form>
 
 							<div>
-								<div className="relative mt-10">
+								<div className="relative my-6">
 									<div aria-hidden="true" className="absolute inset-0 flex items-center">
 										<div className="w-full border-t border-gray-200" />
 									</div>
 									<div className="relative flex justify-center text-sm/6 font-medium">
-										<span className="bg-white px-6 text-gray-900">Or continue with</span>
+										<span className="bg-gray-100 px-6 text-gray-800 dark:bg-gray-950 dark:text-white">
+											Or continue with
+										</span>
 									</div>
 								</div>
 
-								<div className="mt-6 grid grid-cols-2 gap-4">
-									<Button
-										outline
-										className="flex w-full items-center justify-center gap-3 rounded-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
-									>
-										<GoogleIcon />
-										<span className="text-sm/6 font-semibold">Google</span>
-									</Button>
-
-									<Button
-										outline
-										className="flex w-full items-center justify-center gap-3 rounded-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
-									>
-										<GithubIcon className="text-white" />
-										<span className="text-sm/6 font-semibold">GitHub</span>
-									</Button>
-								</div>
+								<Button
+									outline
+									className="flex w-full items-center justify-center gap-3 rounded-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:ring-transparent"
+								>
+									<GoogleIcon />
+									<span className="text-sm/6 font-semibold">Google</span>
+								</Button>
 							</div>
 						</div>
 					</div>
