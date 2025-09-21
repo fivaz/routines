f36f1d6b998b66404d60513c2d99b883e09c4326
fivaz <31249205+fivaz@users.noreply.github.com>
2025-09-21 19:21:57 +0200

    prevent zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,20 @@ import './globals.css';
 import { type ReactNode } from 'react';
 import { PromptProvider } from '@/lib/prompt-context';
 import { APP_NAME } from '@/lib/consts';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 import { AuthController } from '@/app/AuthController';
 import { Provider } from 'jotai';
 import { ThemeProvider } from 'next-themes';
 
 export const metadata: Metadata = {
 	title: APP_NAME,
+};
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
