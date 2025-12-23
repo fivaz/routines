3396b0e26a372f055b031fbcb686d16e61ef48b4
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-23 14:21:27 +0100

    comment the password reset

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -63,52 +63,52 @@ export function LoginForm() {
 						<Input name="password" type="password" required autoComplete="current-password" />
 					</Field>
 
-					<div className="flex items-center justify-between">
-						<div className="flex gap-3">
-							<div className="flex h-6 shrink-0 items-center">
-								<div className="group grid size-4 grid-cols-1">
-									<input
-										id="remember-me"
-										name="remember-me"
-										type="checkbox"
-										className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-green-600 checked:bg-green-600 indeterminate:border-green-600 indeterminate:bg-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
-									/>
-									<svg
-										fill="none"
-										viewBox="0 0 14 14"
-										className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"
-									>
-										<path
-											d="M3 8L6 11L11 3.5"
-											strokeWidth={2}
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											className="opacity-0 group-has-checked:opacity-100"
-										/>
-										<path
-											d="M3 7H11"
-											strokeWidth={2}
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											className="opacity-0 group-has-indeterminate:opacity-100"
-										/>
-									</svg>
-								</div>
-							</div>
-							<label
-								htmlFor="remember-me"
-								className="block text-sm/6 text-gray-900 dark:text-white"
-							>
-								Remember me
-							</label>
-						</div>
+					{/*<div className="flex items-center justify-between">*/}
+					{/*	<div className="flex gap-3">*/}
+					{/*		<div className="flex h-6 shrink-0 items-center">*/}
+					{/*			<div className="group grid size-4 grid-cols-1">*/}
+					{/*				<input*/}
+					{/*					id="remember-me"*/}
+					{/*					name="remember-me"*/}
+					{/*					type="checkbox"*/}
+					{/*					className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-green-600 checked:bg-green-600 indeterminate:border-green-600 indeterminate:bg-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"*/}
+					{/*				/>*/}
+					{/*				<svg*/}
+					{/*					fill="none"*/}
+					{/*					viewBox="0 0 14 14"*/}
+					{/*					className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25"*/}
+					{/*				>*/}
+					{/*					<path*/}
+					{/*						d="M3 8L6 11L11 3.5"*/}
+					{/*						strokeWidth={2}*/}
+					{/*						strokeLinecap="round"*/}
+					{/*						strokeLinejoin="round"*/}
+					{/*						className="opacity-0 group-has-checked:opacity-100"*/}
+					{/*					/>*/}
+					{/*					<path*/}
+					{/*						d="M3 7H11"*/}
+					{/*						strokeWidth={2}*/}
+					{/*						strokeLinecap="round"*/}
+					{/*						strokeLinejoin="round"*/}
+					{/*						className="opacity-0 group-has-indeterminate:opacity-100"*/}
+					{/*					/>*/}
+					{/*				</svg>*/}
+					{/*			</div>*/}
+					{/*		</div>*/}
+					{/*		<label*/}
+					{/*			htmlFor="remember-me"*/}
+					{/*			className="block text-sm/6 text-gray-900 dark:text-white"*/}
+					{/*		>*/}
+					{/*			Remember me*/}
+					{/*		</label>*/}
+					{/*	</div>*/}
 
-						<div className="text-sm/6">
-							<a href="#" className="font-semibold text-green-600 hover:text-green-500">
-								Forgot password?
-							</a>
-						</div>
-					</div>
+					{/*	<div className="text-sm/6">*/}
+					{/*		<a href="#" className="font-semibold text-green-600 hover:text-green-500">*/}
+					{/*			Forgot password?*/}
+					{/*		</a>*/}
+					{/*	</div>*/}
+					{/*</div>*/}
 
 					<div>
 						<Button type="submit" color="green" className="w-full" isLoading={isEmailLoading}>
