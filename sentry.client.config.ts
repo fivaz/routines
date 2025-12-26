7f9d331b709182cc2410c5d39becf451e3bf5f07
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-27 00:23:22 +0100

    disable sentry in dev

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 if (process.env.NODE_ENV !== 'development') {
 	Sentry.init({
-		dsn: 'https://bc7b0e0d51b1f8b5c5ebf2042f3d83f9@o4508857555550208.ingest.de.sentry.io/4508857556861008',
+		dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
 		enabled: false,
 
 		// Add optional integrations for additional features
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-	dsn: 'https://bc7b0e0d51b1f8b5c5ebf2042f3d83f9@o4508857555550208.ingest.de.sentry.io/4508857556861008',
+	dsn: process.env.NEXT_PUBLIC_SENTRY_DNS,
 
 	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
 	tracesSampleRate: 1,
